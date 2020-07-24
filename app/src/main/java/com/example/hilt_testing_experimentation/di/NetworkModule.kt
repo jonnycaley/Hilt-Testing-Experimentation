310f6229296a31a8a5cba537d6b6449f8ed344c9
Jonathan Caley <jonathan.caley@ocado.com>
2020-07-24 13:19:34 +0100

    Replace yahoo endpoint with pokemon endpoint

--- app/src/main/java/com/example/hilt_testing_experimentation/di/NetworkModule.kt
+++ app/src/main/java/com/example/hilt_testing_experimentation/di/NetworkModule.kt
@@ -1,20 +1,15 @@
 package com.example.hilt_testing_experimentation.di
 
-import android.content.Context
-import com.chuckerteam.chucker.api.ChuckerInterceptor
-import com.example.hilt_testing_experimentation.data.YahooFinanceService
+import com.example.hilt_testing_experimentation.data.PokeApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
-import dagger.hilt.android.qualifiers.ApplicationContext
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.time.Duration
 import java.util.concurrent.TimeUnit
 
 @Module
@@ -22,38 +17,24 @@ import java.util.concurrent.TimeUnit
 object NetworkModule {
 
     @Provides
-    fun providesFinanceService(
+    fun providesPokeApiService(
         okHttpClient: OkHttpClient
-    ): YahooFinanceService {
+    ): PokeApiService {
         return Retrofit.Builder()
             .client(okHttpClient)
-            .baseUrl("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/")
+            .baseUrl("https://pokeapi.co/api/v2/")
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build()
-            .create(YahooFinanceService::class.java)
+            .create(PokeApiService::class.java)
     }
 
     @Provides
-    fun providesOkHttpInterceptor(@ApplicationContext context: Context): OkHttpClient {
+    fun providesOkHttpInterceptor(interceptors: Set<@JvmSuppressWildcards Interceptor>): OkHttpClient {
         return OkHttpClient.Builder().apply {
-            addInterceptor(
-                // key for api
-                Interceptor { chain ->
-                    val builder = chain.request().newBuilder()
-                    builder.header("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
-                    builder.header("x-rapidapi-key", "c8e67c907dmsh416ede28f252ad7p171085jsn05b172a4fae6")
-                    return@Interceptor chain.proceed(builder.build())
-                }
-            )
-            addInterceptor(
-                // provides logs
-                HttpLoggingInterceptor().apply {
-                    level = (HttpLoggingInterceptor.Level.BODY)
-                }
-            )
-            // provide chucker http output
-            addInterceptor(ChuckerInterceptor(context))
+            interceptors.forEach { interceptor ->
+                this.addInterceptor(interceptor)
+            }
         }
             .connectTimeout(5, TimeUnit.SECONDS)
             .readTimeout(5, TimeUnit.SECONDS)
