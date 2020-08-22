64474af65b79ca96946071ed14895fd6acadd833
Caley <jonathan.caley@ocado.com>
2020-08-22 18:15:02 +0100

    Add sprites domain class

---
+++ app/src/main/java/com/example/hilt_testing_experimentation/domain/detailedpokemon/Sprites.kt
@@ -0,0 +1,5 @@
+package com.example.hilt_testing_experimentation.domain.detailedpokemon
+
+data class Sprites(
+    val frontDefault: String?
+)
---
+++ app/src/test/java/com/example/hilt_testing_experimentation/domain/detailedpokemon/SpriteBuilder.kt
@@ -0,0 +1,5 @@
+package com.example.hilt_testing_experimentation.domain.detailedpokemon
+
+object SpriteBuilder {
+    var pikachu = Sprites(frontDefault = "frontDefault")
+}
