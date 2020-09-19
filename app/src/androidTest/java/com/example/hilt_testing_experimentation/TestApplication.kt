e11281349d10b618cb3cbeff0264b562eb92bb6a
Caley <jonathan.caley@ocado.com>
2020-09-19 18:12:09 +0100

    Remove old hilt test application

--- app/src/androidTest/java/com/example/hilt_testing_experimentation/TestApplication.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hilt_testing_experimentation
-
-import dagger.hilt.android.testing.CustomTestApplication
-
-@CustomTestApplication(Application::class)
-interface TestApplication
--- app/src/main/java/com/example/hilt_testing_experimentation/Application.kt
+++
@@ -1,7 +0,0 @@
-package com.example.hilt_testing_experimentation
-
-import android.app.Application
-
-open class Application : Application() {
-    // potential @Inject issue here (see https://github.com/google/dagger/issues/2033#issuecomment-671504986 for context)
-}
