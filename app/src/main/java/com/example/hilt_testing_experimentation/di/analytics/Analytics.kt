98229ca821e79d2f03dfc9d57b38903ef598e85d
Caley <jonathan.caley@ocado.com>
2020-09-19 18:05:39 +0100

    Add basic analytics classes

---
+++ app/src/main/java/com/example/hilt_testing_experimentation/di/analytics/Analytics.kt
@@ -0,0 +1,5 @@
+package com.example.hilt_testing_experimentation.di.analytics
+
+interface Analytics {
+    fun logScreenView(screenView: String)
+}
---
+++ app/src/main/java/com/example/hilt_testing_experimentation/di/analytics/AnalyticsImpl.kt
@@ -0,0 +1,10 @@
+package com.example.hilt_testing_experimentation.di.analytics
+
+import android.util.Log
+import javax.inject.Inject
+
+class AnalyticsImpl @Inject constructor(): Analytics {
+    override fun logScreenView(screenView: String) {
+        Log.d("Analytics","Logging screen view: $screenView")
+    }
+}
