236404218a94b55f99016beaf8d982fda0108823
Jonny Caley <jonathan.caley@ocado.com>
2020-11-08 20:49:13 +0000

    Add common gradle version script to all single source of truth for gradle variables

---
+++ feature-pokemondetail/src/main/java/com/example/feature_pokemondetail/Test.kt
@@ -0,0 +1,11 @@
+package com.example.feature_pokemondetail
+
+import android.util.Log
+
+class Test {
+    companion object {
+        fun printFromModule() {
+            Log.i("PokemonDetail","Whaddup!!!")
+        }
+    }
+}
