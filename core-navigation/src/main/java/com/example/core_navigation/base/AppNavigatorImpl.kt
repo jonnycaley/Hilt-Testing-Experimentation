c1153a950d92abd7eda42a52e8688637cd252e6d
Jonny Caley <jonathan.caley@ocado.com>
2020-11-21 20:40:15 +0000

    Rename to app navigator

--- core-navigation/src/main/java/com/example/core_navigation/base/AppNavigatorImpl.kt
+++ core-navigation/src/main/java/com/example/core_navigation/base/AppNavigatorImpl.kt
@@ -2,7 +2,7 @@ package com.example.core_navigation.base
 
 import com.example.feature_pokemonlist.ui.MainFragment
 
-class BaseNavigatorImpl : AppNavigator {
+class AppNavigatorImpl : AppNavigator {
     override fun toMainFragment(): MainFragment {
         return MainFragment.newInstance()
     }
