802abcbfafb8a83d6281097061bb1d29b4f7fb8e
Jonny Caley <jonathan.caley@ocado.com>
2020-11-29 16:24:16 +0000

    Remove example tests

--- core-navigation/src/androidTest/java/com/example/core_navigation/ExampleInstrumentedTest.kt
+++ core-navigation/src/androidTest/java/com/example/core_navigation/ExampleInstrumentedTest.kt
@@ -14,7 +14,6 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.core_navigation.test", appContext.packageName)
+        assertEquals(false, true)
     }
 }
