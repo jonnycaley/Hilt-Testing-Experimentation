ec545c7afc5c8f55618d0e88061befa05406c903
Jonny Caley <jonathan.caley@ocado.com>
2021-02-02 21:24:00 +0000

    Add parameters to enable transition

--- feature-pokemonlist/src/androidTest/java/com/example/feature_pokemonlist/di/navigation/FakePokemonListNavigator.kt
+++ feature-pokemonlist/src/androidTest/java/com/example/feature_pokemonlist/di/navigation/FakePokemonListNavigator.kt
@@ -1,11 +1,17 @@
 package com.example.feature_pokemonlist.di.navigation
 
-import android.content.Context
+import android.app.Activity
+import android.widget.ImageView
 import com.example.core.domain.DetailedPokemon
 import com.example.feature_pokemonlist.navigation.PokemonListNavigator
 
 class FakePokemonListNavigator : PokemonListNavigator {
-    override fun toPokemonDetail(context: Context, detailedPokemon: DetailedPokemon) {
+    override fun toPokemonDetail(
+        activity: Activity,
+        detailedPokemon: DetailedPokemon,
+        adapterPosition: Int,
+        imageView: ImageView
+    ) {
 
     }
 }
