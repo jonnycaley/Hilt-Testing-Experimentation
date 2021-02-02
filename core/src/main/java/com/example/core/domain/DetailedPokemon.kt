800aa7816f1b74b2455c7cac2cc7f2bc03f7669a
Jonny Caley <jonathan.caley@ocado.com>
2021-02-02 21:23:42 +0000

    Make detail pokemon parcelable to add to intent

--- core/src/main/java/com/example/core/domain/DetailedPokemon.kt
+++ core/src/main/java/com/example/core/domain/DetailedPokemon.kt
@@ -1,6 +1,10 @@
 package com.example.core.domain
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class DetailedPokemon(
     val name: String?,
     val imageUrl: String?
-)
+) : Parcelable
