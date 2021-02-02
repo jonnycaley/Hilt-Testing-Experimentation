fe96fc29813c3a5b0ab722d757ea6acc3e4ca634
Jonny Caley <jonathan.caley@ocado.com>
2021-02-02 21:24:19 +0000

    Load image in pokemon detail

--- feature-pokemondetail/src/main/java/com/example/feature_pokemondetail/PokemonDetailActivity.kt
+++ feature-pokemondetail/src/main/java/com/example/feature_pokemondetail/PokemonDetailActivity.kt
@@ -1,12 +1,22 @@
 package com.example.feature_pokemondetail
 
-import android.content.Context
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.view.ViewCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.example.core.di.analytics.Analytics
 import com.example.core.domain.DetailedPokemon
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_pokemon_detail.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -17,16 +27,63 @@ class PokemonDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_pokemon_detail)
+        supportPostponeEnterTransition()
         analytics.logScreenView("PokemonDetailActivity")
+        val extras = intent.extras
+        val pokemon: DetailedPokemon? = extras!!.getParcelable(EXTRA_POKEMON)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val imageTransitionName = extras.getString(EXTRA_TRANSITION_NAME)
+            image_view.transitionName = imageTransitionName
+        }
+        Glide
+            .with(this)
+            .load(pokemon?.imageUrl)
+            .addListener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    supportStartPostponedEnterTransition()
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    supportStartPostponedEnterTransition()
+                    return true
+                }
+            })
+            .into(image_view)
     }
 
     companion object {
-        fun start(context: Context, pokemon: DetailedPokemon) {
-            val intent = Intent(context, PokemonDetailActivity::class.java)
-            context.startActivity(intent)
 
+        const val EXTRA_POKEMON = "EXTRA_POKEMON"
+        const val EXTRA_TRANSITION_NAME = "EXTRA_TRANSITION_NAME"
+
+        fun start(
+            activity: Activity,
+            pokemon: DetailedPokemon,
+            adapterPosition: Int,
+            imageView: ImageView
+        ) {
+            val intent = Intent(activity, PokemonDetailActivity::class.java)
+            intent.putExtra(EXTRA_POKEMON, pokemon)
+            intent.putExtra(EXTRA_TRANSITION_NAME, ViewCompat.getTransitionName(imageView))
+            val options: ActivityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                activity,
+                imageView,
+                ViewCompat.getTransitionName(imageView) ?: ""
+            )
+            activity.startActivity(intent, options.toBundle())
         }
     }
 }
