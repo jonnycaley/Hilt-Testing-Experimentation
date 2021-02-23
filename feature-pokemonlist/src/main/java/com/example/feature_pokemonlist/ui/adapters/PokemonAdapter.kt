6d9a19ebaf5b9f8d568c2be66f9cf250dcd41cf8
Jonny Caley <jonathan.caley@ocado.com>
2021-02-23 14:40:33 +0000

    Cleanup

--- feature-pokemonlist/src/main/java/com/example/feature_pokemonlist/ui/adapters/PokemonAdapter.kt
+++ feature-pokemonlist/src/main/java/com/example/feature_pokemonlist/ui/adapters/PokemonAdapter.kt
@@ -3,7 +3,8 @@ package com.example.feature_pokemonlist.ui.adapters
 import android.app.Activity
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.view.ViewCompat
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.core.domain.DetailedPokemon
 import com.example.feature_pokemonlist.R
@@ -11,44 +12,53 @@ import com.example.feature_pokemonlist.databinding.ItemPokemonBinding
 import com.example.core.di.analytics.Analytics
 import com.example.core.di.imageloader.ImageLoader
 import com.example.feature_pokemonlist.navigation.PokemonListNavigator
+import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
 
+@FragmentScoped
 class PokemonAdapter @Inject constructor(
     private val analytics: Analytics,
     private val imageLoader: ImageLoader,
     private val pokemonListNavigator: PokemonListNavigator
 ) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
 
-    private val pokemon: MutableList<DetailedPokemon> = mutableListOf()
+    private var pokemonList: MutableList<DetailedPokemon> = mutableListOf()
 
     lateinit var activity: Activity
 
-    fun updateItems(list: List<DetailedPokemon>, activity: Activity) {
+    fun updateItems(pokemon: List<DetailedPokemon>, activity: Activity) {
         this.activity = activity
-        val oldPokemonListSize = pokemon.size
-        val newPokemonListSize = list.size
-        val numberOfNewItems = newPokemonListSize - oldPokemonListSize
-        pokemon.clear()
-        pokemon.addAll(list)
-        notifyItemRangeInserted(oldPokemonListSize, numberOfNewItems)
+        val oldPokemonListSize = pokemonList.size
+        val newPokemon = pokemon.subList(oldPokemonListSize, pokemon.size)
+        pokemonList.addAll(newPokemon)
+        notifyItemRangeInserted(oldPokemonListSize, newPokemon.size)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemBinding = ItemPokemonBinding.inflate(
             LayoutInflater.from(parent.context), parent, false)
 
-        return ViewHolder(itemBinding)
+        return ViewHolder(itemBinding, onItemClick)
+    }
+
+    private val onItemClick = { position: Int, imageView: ImageView, textView: TextView ->
+        pokemonListNavigator.toPokemonDetail(activity, pokemonList[position], imageView, textView)
     }
 
     override fun getItemCount(): Int {
-        return pokemon.size
+        return pokemonList.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(pokemon[position])
+        holder.bind(pokemonList[position])
     }
 
-    inner class ViewHolder(private val binding: ItemPokemonBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: ItemPokemonBinding, onItemClick: (Int, ImageView, TextView) -> Unit): RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setOnClickListener {
+                onItemClick(bindingAdapterPosition, binding.image, binding.name)
+            }
+        }
         fun bind(pokemon: DetailedPokemon) {
             with(pokemon) {
                 binding.name.text = name
@@ -59,11 +69,6 @@ class PokemonAdapter @Inject constructor(
                     imageView = binding.image,
                     onResourceReady = { analytics.logImageView(name) }
                 )
-                binding.root.setOnClickListener {
-                    ViewCompat.setTransitionName(binding.image, "$name-image")
-                    ViewCompat.setTransitionName(binding.name, "$name-text")
-                    pokemonListNavigator.toPokemonDetail(activity, this, binding.image, binding.name)
-                }
             }
         }
     }
