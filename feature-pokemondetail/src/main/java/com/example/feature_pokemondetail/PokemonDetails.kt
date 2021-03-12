426841b3b86fb3c1f851a9ff020480817f5b2514
Jonny Caley <jonathan.caley@ocado.com>
2021-03-12 21:40:12 +0000

    Add two items on each row

--- feature-pokemondetail/src/main/java/com/example/feature_pokemondetail/PokemonDetails.kt
+++ feature-pokemondetail/src/main/java/com/example/feature_pokemondetail/PokemonDetails.kt
@@ -4,43 +4,54 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.preferredSize
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.core.domain.DetailedPokemon
+import com.example.core.domain.Stat
 
 @Composable
 fun PokemonDetails(pokemon: DetailedPokemon) {
     Column {
         LazyColumn {
-            items(items = pokemon.stats) {
-                Row(
-                    modifier = Modifier.apply {
-                    }
-                ) {
-                    Box(
-                        modifier = Modifier.clip(RoundedCornerShape(10.dp)).background(Color.Red)
-                    )
-                    Text("${it.name} ${it.baseStat}")
+            items(items = pokemon.stats.zipWithNext()) {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentSize(Alignment.Center)) {
+                    PokemonStatRow(it)
                 }
             }
         }
     }
 }
 
+@Composable
+fun PokemonStatRow(statPair: Pair<Stat, Stat>) {
+    Box(
+        modifier = Modifier
+            .preferredSize(100.dp, 20.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color.Red)
+    )
+    Box(
+        modifier = Modifier
+            .preferredSize(100.dp, 20.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color.Red)
+    )
+//    Text("${stat.name} ${stat.baseStat}", style = MaterialTheme.typography.body1)
+}
+
 @Preview
 @Composable
-fun PreviewPokemonDetails() {
-    PokemonDetails(pokemon = DetailedPokemon())
+fun PreviewPokemonPokemonStat() {
 }
