dce76c89f2f8d52cf65bd2dffa40c9d0f25c2943
Jonny Caley <jonathan.caley@ocado.com>
2021-03-16 20:22:44 +0000

    Create re-usable composable for stats

--- feature-pokemondetail/src/main/java/com/example/feature_pokemondetail/ui/PokemonDetails.kt
+++ feature-pokemondetail/src/main/java/com/example/feature_pokemondetail/ui/PokemonDetails.kt
@@ -2,12 +2,11 @@ package com.example.feature_pokemondetail.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.preferredSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,37 +14,35 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.core.domain.DetailedPokemon
 import com.example.core.domain.Stat
 
 @Composable
 fun PokemonDetails(pokemon: DetailedPokemon) {
-    Column {
-        LazyColumn {
-            items(items = pokemon.stats.zipWithNext()) {
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentSize(Alignment.Center)) {
-                    PokemonStatRow(it)
-                }
-            }
+    LazyColumn {
+        items(pokemon.stats.zipWithNext()) { statPair ->
+            PokemonStatRow(statPair)
         }
     }
 }
 
 @Composable
 fun PokemonStatRow(statPair: Pair<Stat, Stat>) {
-    PokemonStat(statPair.first)
-    PokemonStat(statPair.second)
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight()) {
+        val modifierWithWeight = Modifier.weight(0.5F)
+        PokemonStat(modifierWithWeight, statPair.first)
+        PokemonStat(modifierWithWeight, statPair.second)
+    }
 }
 
 @Composable
-fun PokemonStat(stat: Stat) {
+fun PokemonStat(modifier: Modifier, stat: Stat) {
     Box(
-        modifier = Modifier
-            .preferredSize(150.dp, 30.dp)
+        modifier = modifier
+            .wrapContentHeight()
             .clip(RoundedCornerShape(10.dp))
             .background(Color.Red)
     ) {
@@ -54,9 +51,4 @@ fun PokemonStat(stat: Stat) {
             color = Color.White
         )
     }
-}
-
-@Preview
-@Composable
-fun PreviewPokemonPokemonStat() {
 }
