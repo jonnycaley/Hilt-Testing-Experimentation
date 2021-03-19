a71c23fac3553821842cd247790467c350e502e1
Jonny Caley <jonathan.caley@ocado.com>
2021-03-19 22:07:07 +0000

    Add theme

---
+++ feature-pokemondetail/src/main/java/com/example/feature_pokemondetail/ui/Theme.kt
@@ -0,0 +1,32 @@
+package com.example.feature_pokemondetail.ui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColors = darkColors(
+
+)
+
+private val LightColors = lightColors(
+
+)
+
+@Composable
+fun Theme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColors
+    } else {
+        LightColors
+    }
+
+    MaterialTheme(colors = colors) {
+        content()
+    }
+}
